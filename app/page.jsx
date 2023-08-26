1241c32d09008cee1193de2d7754f277c4b4f3b6
Yura Dmytruk <study.on.work@gmail.com>
2023-08-26 14:51:01 +0300

    Add page about

--- app/page.jsx
+++ app/page.jsx
@@ -2,5 +2,10 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>ksdfjlks;dfkf;dlksjgit </p>
+    </>
+  );
 }
